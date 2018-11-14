5b26a9b1b00c38302db8f813b20b93af8330e9ef
Shishir Chaturvedi <schaturvedi@pandora.com>
2018-11-14 09:17:11 -0500

    the first commit

---
+++ src/main/java/com/smaple/poc/protobuf/SampleMain.java
@@ -0,0 +1,21 @@
+package com.smaple.poc.protobuf;
+
+import example.simple.Simple.SimpleMessage;
+
+import java.util.Arrays;
+
+public class SampleMain {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    SimpleMessage.Builder builder = SimpleMessage.newBuilder();
+    builder
+        .setId(100)
+        .setIsSimple(true)
+        .setName("XYZ")
+        .addAllSampleList(Arrays.asList(1, 2, 3, 4, 5));
+
+    System.out.println(builder.toString());
+    System.out.println(builder.build());
+  }
+}
